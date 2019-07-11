8dfb4db67cf9f6cb6098d4494d7158de56072ce6
Salman.C.Zhang <Salman.C.Zhang@look.com>
2019-07-11 18:04:06 +0800

    support set volume

--- android/src/main/java/com/magugi/plugin/volume_watcher/VolumeChangeObserver.java
+++ android/src/main/java/com/magugi/plugin/volume_watcher/VolumeChangeObserver.java
@@ -61,10 +61,14 @@ public class VolumeChangeObserver {
      */
     public void setVolume(int volume){
         if(mAudioManager != null){
-            // 设置音量
-            mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
-            if(volume<1){
-                mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER,  0);
+            try{
+                // 设置音量
+                mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, volume, 0);
+                if(volume<1){
+                    mAudioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_LOWER,  0);
+                }
+            }catch (Exception ex){
+                //禁止日志
             }
         }
     }
